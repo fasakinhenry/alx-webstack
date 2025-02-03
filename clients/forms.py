51dbec6423898f8e105d0e157493cd79ccc0d3cc
stringpappy <jejeodunayo70@gmail.com>
2025-02-03 11:59:38 +0100

    ficing pycodestyle errors

--- clients/forms.py
+++ clients/forms.py
@@ -15,16 +15,20 @@ class UserRegistrationForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
+
 class ProfileEditForm(forms.ModelForm):
     """A form to edit a user's profile.
     """
+
     class Meta:
         model = Profile
-        fields = ['bio', 'phone_number', 'organisation', 'email', 'address', 'full_name', 'position', 'profile_picture']
+        fields = ['bio', 'phone_number', 'organisation', \
+                'email', 'address', 'full_name', 'position', 'profile_picture']
 
     def __init__(self, *args, **kwargs):
         super(ProfileEditForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control custom-form-control'
             field.widget.attrs['placeholder'] = field.label
-            field.widget.attrs['style'] = 'margin-bottom: 1rem; padding: 0.75rem; border-radius: 0.5rem;'
+            field.widget.attrs['style'] = 'margin-bottom: 1rem;\
+                    padding: 0.75rem; border-radius: 0.5rem;'
