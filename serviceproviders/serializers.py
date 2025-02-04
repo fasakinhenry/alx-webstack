d352ba7854507b60b1a368668b24a0f89f666c4f
rallmac <tobijahekperikpe@gmail.com>
2025-02-04 18:12:21 +0100

    pycodestyle for serializers.py

--- serviceproviders/serializers.py
+++ serviceproviders/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 from .models import Profile
 
+
 class ProfileSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField()
 
     class Meta:
         model = Profile
-        fields = ['id', 'user', 'bio', 'phone_number', 'skills', 'profile_picture']
+        fields = [
+                'id',
+                'user',
+                'bio',
+                'phone_number',
+                'skills',
+                'profile_picture'
+            ]
